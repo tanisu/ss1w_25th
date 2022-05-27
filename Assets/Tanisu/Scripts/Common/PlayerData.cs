19f5a94d10f22423cb6afaf7058ca4cc1e02643f
tanisu <walk.away.renee.5125@gmail.com>
2022-05-27 18:19:47 +0900

    プレイヤー選択
    プレイヤー選択

---
+++ Assets/Tanisu/Scripts/Common/PlayerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlayerDatas")]
+public class PlayerData : ScriptableObject
+{
+    public Sprite bord, player, fall,setImage;
+}
---
+++ Assets/Tanisu/Scripts/Common/PlayerSelector.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class PlayerSelector : MonoBehaviour
+{
+    [SerializeField]
+    Button increment, decrement;
+    [SerializeField] float slideRange = 100f;
+    [SerializeField] Transform playerImages;
+    [SerializeField] GameObject locked;
+    [SerializeField] float effectTime;
+    [SerializeField] SpriteRenderer board, player;
+    List<SelectablePlayer> selectablePlayer;
+    List<Image> images;
+    int currentPlayer = 0;
+    bool isMoving;
+    
+
+    void Start()
+    {
+        selectablePlayer = new List<SelectablePlayer>(playerImages.GetComponentsInChildren<SelectablePlayer>());
+        images = new List<Image>();
+        for(int i = 0;i < selectablePlayer.Count; i++)
+        {
+            selectablePlayer[i].transform.localPosition = new Vector3(i * -slideRange, selectablePlayer[i].transform.localPosition.y);
+            images.Add(selectablePlayer[i].GetComponent<Image>());
+        }
+        increment.onClick.AddListener(() => _incrementPlayer());
+        decrement.onClick.AddListener(() => _decrementPlayer());
+        _interactableArrowButton();
+
+        
+    }
+
+    void _incrementPlayer()
+    {
+        
+        if (currentPlayer >= images.Count - 1 || isMoving) return;
+        _slidePlayerView( 1 );
+        _interactableArrowButton();
+    }
+
+    void _decrementPlayer()
+    {
+        if (currentPlayer <= 0 || isMoving) return;
+        _slidePlayerView(-1);
+        _interactableArrowButton();
+
+    }
+    void _interactableArrowButton()
+    {
+        if(currentPlayer == 0)
+        {
+            decrement.interactable = false;
+            return;
+        }
+        if(currentPlayer >= images.Count - 1)
+        {
+            increment.interactable = false;
+            return;
+        }
+        decrement.interactable = true;
+        increment.interactable = true;
+    }
+
+
+    void _slidePlayerView(int _x)
+    {
+        isMoving = true;
+        images[currentPlayer].raycastTarget = false;
+        images[currentPlayer].DOFade(0f, effectTime);
+        currentPlayer += _x;
+        
+        if (selectablePlayer[currentPlayer].IsLocked())
+        {
+            locked.SetActive(true);
+        }
+        else
+        {
+            locked.SetActive(false);
+        }
+        images[currentPlayer].DOFade(1f, effectTime).OnComplete(() => images[currentPlayer].raycastTarget = true);
+        playerImages.transform.DOLocalMoveX(playerImages.localPosition.x + (slideRange * _x )  , effectTime)
+            .OnComplete(()=>isMoving = false);
+    }
+    
+    public void ChangePlayerSprite()
+    {
+        board.sprite = Config.I.selectedPlayerData.bord;
+        player.sprite = Config.I.selectedPlayerData.player;
+    }
+}
