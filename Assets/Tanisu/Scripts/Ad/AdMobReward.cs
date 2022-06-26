5b7157604f5173c997cf1cbc45dd816107bf5034
tanisu <walk.away.renee.5125@gmail.com>
2022-06-26 12:33:33 +0900

    reward
    reward

---
+++ Assets/Tanisu/Scripts/Ad/AdMobReward.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+
+public class AdMobReward : MonoBehaviour
+{
+    [SerializeField] PlayerSelector playerSelector;
+    bool rewardeFlag = false;
+    RewardedAd rewardedAd;
+    string adUnitId,lockedName;
+
+
+    private void Start()
+    {
+#if UNITY_ANDROID
+        adUnitId = "ca-app-pub-4751206041539571/9962469904";
+#else
+        adUnitId = "";
+#endif
+        CreateAndLoadRewardedAd();
+
+    }
+
+
+    void Update()
+    {
+        if (rewardeFlag)
+        {
+            rewardeFlag = false;
+            Debug.Log("Unlocked");
+            playerSelector.UnLockedPlayer(lockedName);
+        }
+    }
+
+    public void CreateAndLoadRewardedAd()
+    {
+        rewardedAd = new RewardedAd(adUnitId);
+        rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+        rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+        rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+        rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+
+        AdRequest request = new AdRequest.Builder().Build();
+        rewardedAd.LoadAd(request);
+        
+    }
+
+    public void ShowAdmobReward(string _name)
+    {
+        if (rewardedAd.IsLoaded())
+        {
+            
+            lockedName = _name;
+            
+            rewardedAd.Show();
+        }
+        else
+        {
+            Debug.Log("not loaded");
+        }
+    }
+
+    public void HandleRewardedAdLoaded(object sender,EventArgs args)
+    {
+        Debug.Log("reward loaded");
+
+    }
+
+    public void HandleRewardedAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        Debug.Log("Failed : " + args.LoadAdError);
+    }
+
+    public void HandleRewardedAdClosed(object sender,EventArgs args)
+    {
+        Debug.Log("Cancel");
+    }
+
+    public void HandleUserEarnedReward(object sender,Reward args)
+    {
+
+        rewardeFlag = true;
+    }
+}
